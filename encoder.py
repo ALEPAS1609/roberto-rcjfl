c43d2d055e369903f0915cc1989928bd864a9398
ALEPAS1609 <alessio.pasquariello76111@gmail.com>
2025-04-04 23:30:13 +0200

    encoder(pre) - ready to test

--- encoder.py
+++ encoder.py
@@ -9,14 +9,28 @@ class Encoder:
         self.gear_ratio = 27
         self.tsample = 0.02
         self.tdisp = 0.5
+        self.weel_diameter = 68 #mm
 
-        self.encoder_lx = RotaryEncoder(24, 25, max_steps=0)
-        self.encoder_rx = RotaryEncoder(24, 25, max_steps=0)
+        self.encoder_lx = RotaryEncoder(16, 26, max_steps=0)
+        self.encoder_rx = RotaryEncoder(5, 6, max_steps=0)
 
-        self.angle = 0
+        self.distance = 0
+        self.distop = False
 
-    async def rpm(self):
-        pass
+    async def Distance(self):
+        step_start = (self.encoder_lx.steps + (self.encoder_rx.steps * -1) / 2)
+        while (self.distop):
+           step = (self.encoder_lx.steps + (self.encoder_rx.steps * -1) / 2)
+           self.distance = (((step - step_start) / self.ppr) / self.gear_ratio) * self.weel_diameter #mm weel diameter
+
+    def getDistance(self, reset):
+        if (reset):
+            self.distop = True
+            self.distop = False
+        
+        print(f"lx: {self.encoder_lx.steps} rx: {self.encoder_rx.steps}")
+        
 
     def cleanup(self):
-        encoder.close()
+        self.encoder_lx.close()
+        self.encoder_rx.close()
